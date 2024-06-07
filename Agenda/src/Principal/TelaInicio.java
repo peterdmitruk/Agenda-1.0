dad7db7e7a3c6ad1803df7169d1a026050aed711
Peter Dmitruk Junior <peterdmitruk@gmail.com>
2024-06-07 00:00:53 -0300

    Comecei o Projeto Hoje do Zero, nesse commit Ja criei algum metodos para iniciar o projeto:
    Metodo Conectar com banco de dados Concluido
    
    Metodo Adicionar Contatos concluido
    
    Metodo Limpar Campos Concluido
    
    Em breve mais atualizações

--- Agenda/src/Principal/TelaInicio.java
+++ Agenda/src/Principal/TelaInicio.java
@@ -6,7 +6,10 @@
 package Principal;
 
 import java.sql.*;
+import javax.swing.JFormattedTextField;
 import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import net.proteanit.sql.DbUtils;
 
 /**
  *
@@ -17,14 +20,18 @@ public class TelaInicio extends javax.swing.JFrame {
     private Connection conexao;
     private PreparedStatement pstm;
     private ResultSet rs;
-    private String sql;
+    
     /**
      * Creates new form TelaInicio
      */
     public TelaInicio() {
         initComponents();
-        createDb();
+        txtTelefone.setFocusLostBehavior(JFormattedTextField.COMMIT);
+        txtRowId.setVisible(false);
+        txtTelefone.setText("00000000000");
+        connectDb();
     }
+    
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -39,11 +46,11 @@ public class TelaInicio extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
         jScrollPane2 = new javax.swing.JScrollPane();
-        jTable2 = new javax.swing.JTable();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
+        tblContatos = new javax.swing.JTable();
+        btnAdd = new javax.swing.JButton();
+        btnUpdate = new javax.swing.JButton();
+        btnDelete = new javax.swing.JButton();
+        btnClear = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
@@ -54,6 +61,7 @@ public class TelaInicio extends javax.swing.JFrame {
         jLabel7 = new javax.swing.JLabel();
         jLabel8 = new javax.swing.JLabel();
         jLabel9 = new javax.swing.JLabel();
+        txtRowId = new javax.swing.JTextField();
 
         jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
 
@@ -66,7 +74,7 @@ public class TelaInicio extends javax.swing.JFrame {
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setText("AGENDA DE CONTATOS");
 
-        jTable2.setModel(new javax.swing.table.DefaultTableModel(
+        tblContatos.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
             },
@@ -82,34 +90,44 @@ public class TelaInicio extends javax.swing.JFrame {
                 return canEdit [columnIndex];
             }
         });
-        jTable2.getTableHeader().setResizingAllowed(false);
-        jTable2.getTableHeader().setReorderingAllowed(false);
-        jScrollPane2.setViewportView(jTable2);
+        tblContatos.getTableHeader().setResizingAllowed(false);
+        tblContatos.getTableHeader().setReorderingAllowed(false);
+        jScrollPane2.setViewportView(tblContatos);
 
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Principal/create.png"))); // NOI18N
-        jButton1.setToolTipText("ADICIONAR");
-        jButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnAdd.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Principal/create.png"))); // NOI18N
+        btnAdd.setToolTipText("ADICIONAR");
+        btnAdd.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnAdd.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAddActionPerformed(evt);
+            }
+        });
 
-        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Principal/read.png"))); // NOI18N
-        jButton2.setToolTipText("PESQUISAR");
-        jButton2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnUpdate.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Principal/update.png"))); // NOI18N
+        btnUpdate.setToolTipText("ALTERAR");
+        btnUpdate.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
 
-        jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Principal/update.png"))); // NOI18N
-        jButton3.setToolTipText("ALTERAR");
-        jButton3.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnDelete.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Principal/delete.png"))); // NOI18N
+        btnDelete.setToolTipText("EXCLUIR");
+        btnDelete.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
 
-        jButton4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Principal/apagador.png"))); // NOI18N
-        jButton4.setToolTipText("LIMPAR CAMPOS");
-        jButton4.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnClear.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Principal/apagador.png"))); // NOI18N
+        btnClear.setToolTipText("LIMPAR CAMPOS");
+        btnClear.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnClear.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnClearActionPerformed(evt);
+            }
+        });
 
         jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel3.setText("Adicionar");
 
         jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel4.setText("Pesquisar");
+        jLabel4.setText("Alterar");
 
         jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel5.setText("Alterar");
+        jLabel5.setText("Excluir");
 
         jLabel6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel6.setText("Limpar Campos");
@@ -147,24 +165,24 @@ public class TelaInicio extends javax.swing.JFrame {
                     .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(btnAdd, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                         .addGap(24, 24, 24)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(btnUpdate, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 30, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(btnDelete, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(15, 15, 15)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
-                                .addGap(12, 12, 12)
-                                .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(26, 26, 26)
+                                .addComponent(btnClear, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(layout.createSequentialGroup()
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jLabel6))))
+                                .addGap(18, 18, 18)
+                                .addComponent(jLabel6)))
+                        .addGap(7, 7, 7))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                             .addComponent(jLabel9, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -176,7 +194,8 @@ public class TelaInicio extends javax.swing.JFrame {
                             .addComponent(txtNome)
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(txtTelefone, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(0, 0, Short.MAX_VALUE)))))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(txtRowId, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE)))))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -195,7 +214,8 @@ public class TelaInicio extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(txtTelefone, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel8))
+                    .addComponent(jLabel8)
+                    .addComponent(txtRowId, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(txtPesquisar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -207,21 +227,31 @@ public class TelaInicio extends javax.swing.JFrame {
                     .addComponent(jLabel5)
                     .addComponent(jLabel6))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnUpdate, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnDelete, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnClear, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnAdd, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap())
         );
 
-        setSize(new java.awt.Dimension(433, 887));
+        setSize(new java.awt.Dimension(437, 887));
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void txtPesquisarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtPesquisarActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_txtPesquisarActionPerformed
+
+    private void btnAddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAddActionPerformed
+        //CHAMA O METODO PARA ADICIONAR UM CONTATO
+        addContato();
+    }//GEN-LAST:event_btnAddActionPerformed
+
+    private void btnClearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnClearActionPerformed
+        //CHAMA METODO PARA LIMPAR TODOS OS CAMPOS
+        clearCampos();
+    }//GEN-LAST:event_btnClearActionPerformed
 
     /**
      * @param args the command line arguments
@@ -259,10 +289,10 @@ public class TelaInicio extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
+    private javax.swing.JButton btnAdd;
+    private javax.swing.JButton btnClear;
+    private javax.swing.JButton btnDelete;
+    private javax.swing.JButton btnUpdate;
     private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
@@ -274,31 +304,54 @@ public class TelaInicio extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTable jTable2;
+    private javax.swing.JTable tblContatos;
     private javax.swing.JTextField txtNome;
     private javax.swing.JTextField txtPesquisar;
+    private javax.swing.JTextField txtRowId;
     private javax.swing.JFormattedTextField txtTelefone;
     // End of variables declaration//GEN-END:variables
 
-private void createDb(){
-    
-    sql = "jdbc:sqlite:contatos.db";
+private void connectDb(){
+    String sql2 = "CREATE TABLE IF NOT EXISTS contatos(nome VARCHAR(40) NOT NULL, telefone VARCHAR(20) NOT NULL)";
+    String sql = "jdbc:sqlite:contatos.db";
     try{
     conexao = DriverManager.getConnection(sql);
+    pstm = conexao.prepareStatement(sql2);
+    pstm.execute();
     }catch(Exception e){
-        JOptionPane.showMessageDialog(null,"ERRO AO CONECTAR COM BANCO DE DADOS" + e);
+        JOptionPane.showMessageDialog(null,"ERRO AO CONECTAR COM BANCO DE DADOS -> " + e);
     }
 }
-
-private void createTable(){
-    sql = "CREATE TABLE contatos (nome VARCHAR(40) NOT NULL, telefone VARCHAR(20) NOT NULL)";
+    private void addContato(){
+        String sql = "INSERT INTO contatos (nome, telefone) values(?,?)";
+        try {
+            connectDb();
+        pstm = conexao.prepareStatement(sql);
+        pstm.setString(1, txtNome.getText());
+        pstm.setString(2, txtTelefone.getText());
+        pstm.executeUpdate();
+        JOptionPane.showMessageDialog(null, "Contato Adicionado Com Sucesso!");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"ERRO AO ADICIONAR CONTATO -> "+ e);
+        }
+    }
+    private void updateContatos(){
     
-    try {
-        
-    } catch (Exception e) {
+    }
+    private void deleteContatos(){
+    
     }
     
+    private void clearCampos(){
+        txtNome.setText(null);
+        txtTelefone.setText("00000000000");
+        txtPesquisar.setText(null);
+        txtRowId.setText(null);
+        btnAdd.setEnabled(true);
+    }
     
-}
+    private void preencherTable(){
+    DefaultTableModel tabela = (DefaultTableModel) tblContatos.getModel();
     
+    }
 }
